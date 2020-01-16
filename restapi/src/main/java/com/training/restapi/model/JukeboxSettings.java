4c173a2db5bd5899c474133a3abf9fe1098b98f2
Leonce Bali <jaimerajkat@gmail.com>
2020-01-15 22:02:00 -0500

    controller initiated

--- restapi/src/main/java/com/training/restapi/model/JukeboxSettings.java
+++ restapi/src/main/java/com/training/restapi/model/JukeboxSettings.java
@@ -5,5 +5,12 @@ import java.util.List;
 public class JukeboxSettings{
 
     private List<Settings> settings;
-    
+
+    public List<Settings> getSettings() {
+        return settings;
+    }
+
+    public void setSettings(List<Settings> settings) {
+        this.settings = settings;
+    }
 }
